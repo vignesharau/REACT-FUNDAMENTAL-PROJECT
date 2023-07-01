44972dc3e348f1f256266eeac8e04373e1abf86a
vignesharau <vignesharau@gmail.com>
2023-07-01 13:27:49 +0530

    first commit

---
+++ 03-reviews/src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import people from "./data";
+const App = () => {
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+  return <h2>Reviews Starter</h2>;
+};
+export default App;
