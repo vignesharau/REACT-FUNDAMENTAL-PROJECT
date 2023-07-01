0a966a497ad63eb831e4e9ba0940a8bc51ea5855
vignesharau <vignesharau@gmail.com>
2023-07-01 16:32:02 +0530

    commit

--- 05-menu/src/App.jsx
+++ 05-menu/src/App.jsx
@@ -1,4 +1,30 @@
+import Title from "./Title";
+import { useState } from "react";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu">
+        <Title text="Our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
