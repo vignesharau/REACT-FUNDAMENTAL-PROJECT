b8e19b5eed7784ccff7485aa5a172051d7c6d570
vignesharau <vignesharau@gmail.com>
2023-07-01 13:13:45 +0530

    first commit

--- 02-tours/src/component/Tours.jsx
+++ 02-tours/src/component/Tours.jsx
@@ -1,4 +1,19 @@
-const Tours = () => {
-  return <div>Tours</div>;
+import Tour from "./Tour";
+
+const Tours = ({ tours,removeTour }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our tours</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="tours">
+        {tours.map((tour)=>{
+
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+        })}
+      </div>
+    </section>
+  );
 };
 export default Tours;
