01db99ac844bae724eaa300e718c78aa79bdaacb
vignesharau <vignesharau@gmail.com>
2023-07-05 13:29:53 +0530

    final commit

--- 09-color-generator/src/Form.jsx
+++ 09-color-generator/src/Form.jsx
@@ -1,6 +1,31 @@
-const Form = () => {
+import { useState } from "react";
+
+const Form = (addColor) => {
+  const [color, setColor] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addColor(color);
+  };
   return (
-    <div>Form</div>
-  )
-}
-export default Form
+    <section className="container">
+      <h4>color generator</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="color"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <input
+          type="text"
+          value={color}
+          placeholder="#f15025"
+          onChange={(e) => setColor(e.target.value)}
+        />
+        <button type="submit" className="btn" style={{ background: color }}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+};
+export default Form;
